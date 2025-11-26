c6f9231ae8d7eed91503909db3dfde6ad1dbe285
memparanoid <memparanoid@gmail.com>
2025-11-26 19:22:58 -0300

    feat(memcodec): add empty crate stub

---
+++ crates/memcodec/src/lib.rs
@@ -0,0 +1,5 @@
+// Copyright (c) 2025-2026 Federico Hoerth <memparanoid@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-only
+// See LICENSE in the repository root for full license text.
+
+//! High-performance secure codec with memory zeroization.
