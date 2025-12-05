cb69b3f139444fc8110f6093a3b625a6f9d1a7a8
memparanoid <memparanoid@gmail.com>
2025-12-05 17:53:57 -0300

    chore: remove memcode v1 (replaced by memcodec)

--- crates/memcode/core/src/support/mod.rs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2025-2026 Federico Hoerth <memparanoid@gmail.com>
-// SPDX-License-Identifier: GPL-3.0-only
-// See LICENSE in the repository root for full license text.
-
-#[cfg(any(test, feature = "test_utils"))]
-pub mod test_utils;
