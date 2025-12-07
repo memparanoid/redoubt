5f57282b07e803e0e7195ff1eec27788b5c0087a
memparanoid <memparanoid@gmail.com>
2025-12-07 15:10:24 -0300

    refactor(memvault_core): simplify storage mod with pub use

--- crates/memvault/core/src/master_key/storage/mod.rs
+++ crates/memvault/core/src/master_key/storage/mod.rs
@@ -4,25 +4,14 @@
 
 //! Storage backend implementations
 
-use membuffer::BufferError as MemBufferError;
-
-use crate::BufferError;
-
 #[cfg(not(feature = "no_std"))]
 mod std;
 
 #[cfg(feature = "no_std")]
 mod portable;
 
-pub fn open(
-    f: &mut dyn FnMut(&[u8]) -> Result<(), MemBufferError>,
-) -> Result<(), BufferError> {
-    #[cfg(not(feature = "no_std"))]
-    {
-        std::open(f)
-    }
-    #[cfg(feature = "no_std")]
-    {
-        portable::open(f)
-    }
-}
+#[cfg(not(feature = "no_std"))]
+pub use std::open;
+
+#[cfg(feature = "no_std")]
+pub use portable::open;
