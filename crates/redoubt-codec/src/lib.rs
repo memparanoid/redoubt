3e30c6837946aff4dbef7cdf0f0c6fac36fac8aa
memparanoid <memparanoid@gmail.com>
2025-12-19 21:20:19 -0300

    chore: update copyright headers to Federico Hoerth

--- crates/redoubt-codec/src/lib.rs
+++ crates/redoubt-codec/src/lib.rs
@@ -2,6 +2,9 @@
 // SPDX-License-Identifier: GPL-3.0-only
 // See LICENSE in the repository root for full license text.
 
-#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_std)]// Copyright (c) 2025-2026 Federico Hoerth <memparanoid@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-only
+// See LICENSE in the repository root for full license text.
+
 pub use redoubt_codec_core::*;
 pub use redoubt_codec_derive::*;
