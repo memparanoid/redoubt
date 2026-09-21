7f3fc38c506eadc8fcfccfbeedda97345281fbef
memparanoid <memparanoid@gmail.com>
2026-09-21 08:17:20 -0300

    refactor(redoubt-buffer): migrate crates/redoubt-buffer/src/tests/page.rs::test_mark_dontdump_succeeds to Result<(), Box<dyn Error>>

--- crates/redoubt-buffer/src/tests/page.rs
+++ crates/redoubt-buffer/src/tests/page.rs
@@ -177,10 +177,12 @@ mod page_tests {
     #[cfg(target_os = "linux")]
     #[test]
     #[cfg_attr(not(miri), serial(page))]
-    fn test_mark_dontdump_succeeds() {
-        let page = Page::new().expect("Failed to new()");
+    fn test_mark_dontdump_succeeds() -> Result<(), Box<dyn std::error::Error>> {
+        let page = Page::new()?;
 
-        page.mark_dontdump().expect("Failed to mark_dontdump()");
+        page.mark_dontdump()?;
+
+        Ok(())
     }
 
     #[cfg(target_os = "linux")]
