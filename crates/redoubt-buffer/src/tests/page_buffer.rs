1b0f72f29c60b4c3cf4ca04a6649117a185bdd1e
memparanoid <memparanoid@gmail.com>
2026-09-21 08:17:27 -0300

    refactor(redoubt-buffer): migrate crates/redoubt-buffer/src/tests/page_buffer.rs::subprocess_test_open_mut_aborts_on_protect_failure to Result<(), Box<dyn Error>>

--- crates/redoubt-buffer/src/tests/page_buffer.rs
+++ crates/redoubt-buffer/src/tests/page_buffer.rs
@@ -378,14 +378,16 @@ mod page_buffer_tests {
 
         #[test]
         #[ignore]
-        fn subprocess_test_open_mut_aborts_on_protect_failure() {
-            let mut buffer =
-                PageBuffer::new(ProtectionStrategy::MemProtected, 32).expect("Failed to new(..)");
+        fn subprocess_test_open_mut_aborts_on_protect_failure()
+        -> Result<(), Box<dyn std::error::Error>> {
+            let mut buffer = PageBuffer::new(ProtectionStrategy::MemProtected, 32)?;
 
             let _ = buffer.open_mut(&mut |_bytes| {
                 block_mprotect();
                 Ok(())
             });
+
+            Ok(())
         }
 
         #[test]
